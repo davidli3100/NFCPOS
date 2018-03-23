9338572c8c81ca0e0345e50f7092721e3a43c0dc
Jason Huang <jhthenerd@users.noreply.github.com>
2018-03-23 19:21:37 -0400

    goin back

--- simplona/pages/nfc.js
+++ simplona/pages/nfc.js
@@ -16,6 +16,243 @@ import {
   Button,
   DeviceEventEmitter
 } from 'react-native';
+<<<<<<< HEAD
+=======
+import NfcManager, {NdefParser} from 'react-native-nfc-manager';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            supported: true,
+            enabled: false,
+            isWriting: false,
+            urlToWrite: 'google.com',
+            tag: {},
+        }
+    }
+
+    componentDidMount() {
+        NfcManager.isSupported()
+            .then(supported => {
+                this.setState({ supported });
+                if (supported) {
+                    this._startNfc();
+                }
+            })
+    }
+
+    componentWillUnmount() {
+        if (this._stateChangedSubscription) {
+            this._stateChangedSubscription.remove();
+        }
+    }
+
+    render() {
+        let { supported, enabled, tag, isWriting, urlToWrite} = this.state;
+        return (
+            <ScrollView style={{flex: 1}}>
+                <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                    <Text>{`Is NFC supported ? ${supported}`}</Text>
+                    <Text>{`Is NFC enabled (Android only)? ${enabled}`}</Text>
+
+                    <TouchableOpacity style={{ marginTop: 20 }} onPress={this._startDetection}>
+                        <Text style={{ color: 'blue' }}>Detect Tags</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={{ marginTop: 20 }} onPress={this._stopDetection}>
+                        <Text style={{ color: 'red' }}>Stop Tag Detection</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={{ marginTop: 20 }} onPress={this._clearMessages}>
+                        <Text>Clear</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={{ marginTop: 20 }} onPress={this._goToNfcSetting}>
+                        <Text >(android) Go to NFC setting</Text>
+                    </TouchableOpacity>
+
+                    {
+                        <View style={{padding: 10, marginTop: 20, backgroundColor: '#e0e0e0'}}>
+                            <Text>(android) Write NDEF Test</Text>
+                            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                                <Text>http://www.</Text>
+                                <TextInput
+                                    style={{width: 200}}
+                                    value={urlToWrite}
+                                    onChangeText={urlToWrite => this.setState({ urlToWrite })}
+                                />
+                            </View>
+
+                            <TouchableOpacity 
+                                style={{ marginTop: 20, borderWidth: 1, borderColor: 'blue', padding: 10 }} 
+                                onPress={isWriting ? this._cancelNdefWrite : this._requestNdefWrite}>
+                                <Text style={{color: 'blue'}}>{`(android) ${isWriting ? 'Cancel' : 'Write NDEF'}`}</Text>
+                            </TouchableOpacity>
+
+                            <TouchableOpacity 
+                                style={{ marginTop: 20, borderWidth: 1, borderColor: 'blue', padding: 10 }} 
+                                onPress={isWriting ? this._cancelNdefWrite : this._requestFormat}>
+                                <Text style={{color: 'blue'}}>{`(android experimental) ${isWriting ? 'Cancel' : 'Format'}`}</Text>
+                            </TouchableOpacity>
+                        </View>
+                    }
+
+                    <Text style={{ marginTop: 20 }}>{`Current tag JSON: ${JSON.stringify(tag)}`}</Text>
+                </View>
+            </ScrollView>
+        )
+    }
+
+    _requestFormat = () => {
+        let {isWriting} = this.state;
+        if (isWriting) {
+            return;
+        }
+
+        this.setState({isWriting: true});
+        NfcManager.requestNdefWrite(null, {format: true})
+            .then(() => console.log('format completed'))
+            .catch(err => console.warn(err))
+            .then(() => this.setState({isWriting: false}));
+    }
+
+    _requestNdefWrite = () => {
+        function strToBytes(str) {
+            let result = [];
+            for (let i = 0; i < str.length; i++) {
+                result.push(str.charCodeAt(i));
+            }
+            return result;
+        }
+
+        let {isWriting, urlToWrite} = this.state;
+        if (isWriting) {
+            return;
+        }
+
+        const urlBytes = strToBytes(urlToWrite);
+        const headerBytes = [0xD1, 0x01, (urlBytes.length + 1), 0x55, 0x01];
+        const bytes = [...headerBytes, ...urlBytes];
+
+        this.setState({isWriting: true});
+        NfcManager.requestNdefWrite(bytes)
+            .then(() => console.log('write completed'))
+            .catch(err => console.warn(err))
+            .then(() => this.setState({isWriting: false}));
+    }
+
+    _cancelNdefWrite = () => {
+        this.setState({isWriting: false});
+        NfcManager.cancelNdefWrite()
+            .then(() => console.log('write cancelled'))
+            .catch(err => console.warn(err))
+    }
+
+    _startNfc() {
+        NfcManager.start({
+            onSessionClosedIOS: () => {
+                console.log('ios session closed');
+            }
+        })
+            .then(result => {
+                console.log('start OK', result);
+            })
+            .catch(error => {
+                console.warn('start fail', error);
+                this.setState({supported: false});
+            })
+
+        if (Platform.OS === 'android') {
+            NfcManager.getLaunchTagEvent()
+                .then(tag => {
+                    console.log('launch tag', tag);
+                    if (tag) {
+                        this.setState({ tag });
+                    }
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+            NfcManager.isEnabled()
+                .then(enabled => {
+                    this.setState({ enabled });
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+            NfcManager.onStateChanged(
+                event => {
+                    if (event.state === 'on') {
+                        this.setState({enabled: true});
+                    } else if (event.state === 'off') {
+                        this.setState({enabled: false});
+                    } else if (event.state === 'turning_on') {
+                        // do whatever you want
+                    } else if (event.state === 'turning_off') {
+                        // do whatever you want
+                    }
+                }
+            )
+                .then(sub => {
+                    this._stateChangedSubscription = sub; 
+                    // remember to call this._stateChangedSubscription.remove()
+                    // when you don't want to listen to this anymore
+                })
+                .catch(err => {
+                    console.warn(err);
+                })
+        }
+    }
+
+    _onTagDiscovered = tag => {
+        console.log('Tag Discovered', tag);
+        this.setState({ tag });
+        let url = this._parseUri(tag);
+        if (url) {
+            Linking.openURL(url)
+                .catch(err => {
+                    console.warn(err);
+                })
+        }
+    }
+
+    _startDetection = () => {
+        NfcManager.registerTagEvent(this._onTagDiscovered)
+            .then(result => {
+                console.log('registerTagEvent OK', result)
+            })
+            .catch(error => {
+                console.warn('registerTagEvent fail', error)
+            })
+    }
+
+    _stopDetection = () => {
+        NfcManager.unregisterTagEvent()
+            .then(result => {
+                console.log('unregisterTagEvent OK', result)
+            })
+            .catch(error => {
+                console.warn('unregisterTagEvent fail', error)
+            })
+    }
+
+    _clearMessages = () => {
+        this.setState({tag: null});
+    }
+
+    _goToNfcSetting = () => {
+        if (Platform.OS === 'android') {
+            NfcManager.goToNfcSetting()
+                .then(result => {
+                    console.log('goToNfcSetting OK', result)
+                })
+                .catch(error => {
+                    console.warn('goToNfcSetting fail', error)
+                })
+        }
+    }
+>>>>>>> parent of 82628c2... not working, pre-new nfc lib
 
 export default class NfcSample extends Component {
   readTagId() {
