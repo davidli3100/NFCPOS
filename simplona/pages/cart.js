be7b7ce941fe1f50dc1ca260aba51f7a6f2bf53d
davidli3100 <david.li3100@gmail.com>
2018-03-23 22:07:58 -0400

    push cart to git

--- simplona/pages/cart.js
+++ simplona/pages/cart.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
+import {ToastAndroid} from "react-native";
+import { Image } from 'react-native';
+import NFC, {NfcDataType, NdefRecordType} from "react-native-nfc";
+import NfcManager, {NdefParser} from 'react-native-nfc-manager';
+import './nfc.js';
 import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text } from 'native-base';
 export default class AnatomyExample extends Component {
+
+  
   render() {
     return (
       <Container>
@@ -11,13 +18,59 @@ export default class AnatomyExample extends Component {
             </Button>
           </Left>
           <Body>
-            <Title>Header</Title>
+            <Title>Cart</Title>
           </Body>
           <Right />
         </Header>
         <Content>
           <Text>
-            This is Cart
+            <CardItem>
+              <Left>
+                if(upc == "gum") {
+                <Thumbnail large source={{uri: './assets/gum.png'}} />
+                }
+
+                if(upc == "mouse") {
+                <Thumbnail large source={{uri: './assets/mouse.png'}} />
+                }
+
+                if(upc == "paper") {
+                <Thumbnail large source={{uri: './assets/paper.png'}} />
+                }
+              </Left>
+            </CardItem>
+         
+
+               for (let i = 0; i < upc.length; i++) { 
+                if (upc == "gum") {
+                  <CardItem>
+                  <Body>
+                <Text>Gum</Text>
+                <Text note>$1.99</Text>
+                </Body>
+                </CardItem>
+                 }
+
+                if (upc == "mouse") {
+                  <CardItem>
+                  <Body>
+                <Text>Mouse</Text>
+                <Text note>$24.99</Text>
+                </Body>
+                </CardItem>
+                }
+
+                if (upc == "paper") {
+                  <CardItem>
+                  <Body>
+                <Text>Paper</Text>
+                <Text note>$4.99</Text>
+                </Body>
+                </CardItem>
+                }
+              }
+              { descriptors }
+    
           </Text>
         </Content>
         <Footer>
