2248130f10f398a5e4b90aeeacd00b69987bdca1
Jason Huang <jhthenerd@users.noreply.github.com>
2018-03-23 22:45:22 -0400

    k have fun m8

--- simplona/pages/cart.js
+++ simplona/pages/cart.js
@@ -6,9 +6,18 @@ import NfcManager, {NdefParser} from 'react-native-nfc-manager';
 import './nfc.js';
 import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text } from 'native-base';
 export default class AnatomyExample extends Component {
-
+  componentDidMount() {
+    NfcManager.isSupported()
+        .then(supported => {
+            this.setState({ supported });
+            if (supported) {
+                this._startNfc();
+            }
+        })
+  }
   
   render() {
+    this._startDetection
     return (
       <Container>
         <Header>
@@ -106,4 +115,93 @@ like gum it'll automatically add a gum card item to descriptors */
       </Container>
     );
   }
+  
+  _startNfc() {
+    NfcManager.start({
+        onSessionClosedIOS: () => {
+            console.log('ios session closed');
+        }
+    })
+        .then(result => {
+            console.log('start OK', result);
+        })
+        .catch(error => {
+            console.warn('start fail', error);
+            this.setState({supported: false});
+        })
+
+    if (Platform.OS === 'android') {
+        NfcManager.getLaunchTagEvent()
+            .then(tag => {
+                console.log('launch tag', tag);
+                if (tag) {
+                    this.setState({ tag });
+                }
+            })
+            .catch(err => {
+                console.log(err);
+            })
+        NfcManager.isEnabled()
+            .then(enabled => {
+                this.setState({ enabled });
+            })
+            .catch(err => {
+                console.log(err);
+            })
+        NfcManager.onStateChanged(
+            event => {
+                if (event.state === 'on') {
+                    this.setState({enabled: true});
+                } else if (event.state === 'off') {
+                    this.setState({enabled: false});
+                } else if (event.state === 'turning_on') {
+                    // do whatever you want
+                } else if (event.state === 'turning_off') {
+                    // do whatever you want
+                }
+            }
+        )
+            .then(sub => {
+                this._stateChangedSubscription = sub; 
+                // remember to call this._stateChangedSubscription.remove()
+                // when you don't want to listen to this anymore
+            })
+            .catch(err => {
+                console.warn(err);
+            })
+    }
+}
+
+_onTagDiscovered = tag => {
+    console.log('Tag Discovered', tag);
+    this.setState({ tag });
+    let upc = this._parseText(tag);
+    if (url) {
+        Linking.openURL(url)
+            .catch(err => {
+                console.warn(err);
+            })
+    }
+}
+
+_startDetection = () => {
+    NfcManager.registerTagEvent(this._onTagDiscovered)
+        .then(result => {
+            console.log('registerTagEvent OK', result)
+        })
+        .catch(error => {
+            console.warn('registerTagEvent fail', error)
+        })
+}
+_parseText = (tag) => {
+  if (tag.ndefMessage) {
+      let result = NdefParser.parseUri(tag.ndefMessage[0]),
+          uri = result && result.uri;
+      if (uri) {
+          console.log('parseUri: ' + uri);
+          return uri;
+      }
+  }
+  return null;
+}
 }
